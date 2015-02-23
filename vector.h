edd9c6f21bc719d98528d50ae8dc25fa365f2e50
Mingcheng Chen <linyufly@linyufly-desktop.(none)>
2015-02-23 14:33:31 -0600

    Updated vector.h

--- vector.h
+++ vector.h
@@ -3,14 +3,84 @@
 #ifndef VECTOR_H_
 #define VECTOR_H_
 
+#include <cmath>
+
+namespace {
+
+template<typename T>
+T square(T value) {
+  return value * value;
+}
+
+}
+
+template<typename T, int D>
 class Vector {
  public:
-  double norm() = 0;
-  double dot(const Vector &) = 0;
-  Vector add(const Vector &) = 0;
-  Vector subtract(const Vector &) = 0;
-  Vector scale_up(double lambda) = 0;
-  Vector scale_down(double lambda) = 0;
+  T norm() const {
+    T result = T(0);
+
+    for (int dimension = 0; dimension < D; dimension++) {
+      result += square<T>(coordinates_[dimension]);
+    }
+
+    return static_cast<T>(sqrt(static_cast<double>(result)));
+  }
+
+  T dot(const Vector<T, D> &other) {
+    T result = T(0);
+
+    for (int dimension = 0; dimension < D; dimension++) {
+      result += coordinates_[dimension] * other.coordinates_[dimension];
+    }
+
+    return result;
+  }
+
+  Vector<T, D> add(const Vector<T, D> &other) {
+    Vector<T, D> result;
+
+    for (int dimension = 0; dimension < D; dimension++) {
+      result.coordinates_[dimension] =
+          coordinates_[dimension] + other.coordinates_[dimension];
+    }
+
+    return result;
+  }
+
+  Vector<T, D> subtract(const Vector<T, D> &other) {
+    Vector<T, D> result;
+
+    for (int dimension = 0; dimension < D; dimension++) {
+      result.coordinates_[dimension] =
+          coordinates_[dimension] - other.coordinates_[dimension];
+    }
+
+    return result;
+  }
+
+  Vector<T, D> scale_up(double lambda) {
+    Vector<T, D> result;
+
+    for (int dimension = 0; dimension < D; dimension++) {
+      result.coordinates_[dimension] = coordinates_[dimension] * lambda;
+    }
+
+    return result;
+  }
+
+  Vector<T, D> scale_down(double lambda) {
+    Vector<T, D> result;
+
+    for (int dimension = 0; dimension < D; dimension++) {
+      result.coordinates_[dimension] = coordinates_[dimension] / lambda;
+    }
+
+    return result;
+  }
+
+ private:
+  T coordinates_[D];
 };
 
 #endif  // VECTOR_H_ 
